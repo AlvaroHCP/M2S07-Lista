85c5c13bf4cb6b2e9c4c297bd301eef40b98b13c
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-11-16 21:54:58 -0300

    Starting the project with Spring Initializer

---
+++ API/src/main/java/org/devinhouse/senai/API/M2S07ListaApplication.java
@@ -0,0 +1,13 @@
+package org.devinhouse.senai.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2S07ListaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2S07ListaApplication.class, args);
+	}
+
+}
---
+++ API/src/test/java/org/devinhouse/senai/API/M2S07ListaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devinhouse.senai.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2S07ListaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
